4359baaee3bc1f43fbbba7ad8c54a4a238b07e09
Joybien <zcgtaipei@gmail.com>
2023-10-26 10:14:06 +0800

    Add files via upload

--- videoPlayer/ViewController.swift
+++ videoPlayer/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    //(1) create av View for AVPlayerViewController
+    @IBOutlet weak var avView: UIView!
+    
+    //(2) declare a AVPlayerViewController
+    var  vc : AVPlayerViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func pause(_ sender: Any) {
+        vc.player!.pause()
+    }
+    @IBAction func play(_ sender: Any) {
+        vc.player!.play()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //(3)
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "IMG_0063", ofType: "MOV")!))
+        
+        vc = AVPlayerViewController()
+        vc.player = player
+        vc.showsPlaybackControls = true
+        
+        //(4)
+        vc.view.frame = avView.bounds
+        avView.addSubview(vc.view)
+    }
 }
 
